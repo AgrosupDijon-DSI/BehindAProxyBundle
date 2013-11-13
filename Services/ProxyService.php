a9a69ece0394308ae2a37b259d1d92880694a6ca
Valérian Girard <valerian.girard@educagri.fr>
2013-11-13 11:51:07 +0100

    Add a test on the validity of a curl resource

--- Services/ProxyService.php
+++ Services/ProxyService.php
@@ -18,12 +18,11 @@ class ProxyService
         $this->parameters = $parameters;
     }
 
-
     /**
      * Retur true if the stream context default is set
      * @return boolean
      */
-    public function setDefaultStreamContext()
+    public function loadDefaultStreamContext()
     {
         if ($this->parameters["enabled"] === true && $this->parameters["load_default_stream_context"]) {
             stream_context_set_default($this->getStreamContext());
@@ -39,17 +38,17 @@ class ProxyService
      */
     public function setProxyForSoapClient(array &$configs)
     {
-            if ($this->parameters["enabled"] === true) {
-                $configs['proxy_host'] = $this->parameters["host"];
-                $configs['proxy_port'] = $this->parameters["port"];
+        if ($this->parameters["enabled"] === true) {
+            $configs['proxy_host'] = $this->parameters["host"];
+            $configs['proxy_port'] = $this->parameters["port"];
 
-                if ($this->parameters["login"] != null) {
-                    $configs['proxy_login'] = $this->parameters["login"];
-                }
-                if ($this->parameters["password"] != null) {
-                    $configs['proxy_password'] = $this->parameters["password"];
-                }
+            if ($this->parameters["login"] != null) {
+                $configs['proxy_login'] = $this->parameters["login"];
             }
+            if ($this->parameters["password"] != null) {
+                $configs['proxy_password'] = $this->parameters["password"];
+            }
+        }
     }
 
     /**
@@ -62,18 +61,17 @@ class ProxyService
      */
     public function setProxyForCURL(&$resource)
     {
-        if (get_resource_type($resource) === "curl") {
+        if (is_resource($resource) && get_resource_type($resource) === "curl") {
 
-            
-                if ($this->parameters["enabled"] === true) {
-                    curl_setopt($resource, CURLOPT_PROXY, $this->parameters["host"]);
-                    curl_setopt($resource, CURLOPT_PROXYPORT, $this->parameters["port"]);
+            if ($this->parameters["enabled"] === true) {
+                curl_setopt($resource, CURLOPT_PROXY, $this->parameters["host"]);
+                curl_setopt($resource, CURLOPT_PROXYPORT, $this->parameters["port"]);
 
-                    if ($this->parameters["login"] != null) {
-                        curl_setopt($resource, CURLOPT_PROXYAUTH, $this->parameters["login"] . ':' . $this->parameters["password"]);
-                    }
+                if ($this->parameters["login"] != null) {
+                    curl_setopt($resource, CURLOPT_PROXYAUTH, $this->parameters["login"] . ':' . $this->parameters["password"]);
                 }
-            
+            }
+
             return true;
         } else {
             throw new \RuntimeException('$resource must be a curl resource');
@@ -91,16 +89,15 @@ class ProxyService
     public function fileGetContent($url)
     {
         $cxContext = null;
-        
+
         if ($this->parameters["enabled"] === true) {
             $context = $this->getStreamContext();
-            
+
             $cxContext = stream_context_create($context);
         }
 
         return file_get_contents($url, false, $cxContext);
     }
-
 
     /**
      * Get configuration for a stream context
